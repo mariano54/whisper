0e9e933e22b8927e22eb85908279b840b5904a4d
Keenon Werling <keenon@stanford.edu>
2013-02-06 20:31:15 -0800

    moved chat box and added js

--- web/js/chat.js
+++ web/js/chat.js
@@ -2,35 +2,51 @@ var chatRecord;
 var chatBox;
 var chatTimer;
 function receiveChat(message,seconds) {
+
     //Drop in the chat box
+
     var newDiv = document.createElement('div');
     newDiv.innerHTML = message;
     newDiv.style.margin = '10px';
+    newDiv.className = "chatRecord";
     chatRecord.appendChild(newDiv);
+
     //Make the div dissappear
+
     newDiv.lifeTime = seconds*30;
+
+    newDiv.addEventListener("mousedown",function() {
+        newDiv.lifeTime = 30;
+    });
+
     var timer = window.setInterval(function() {
-    newDiv.lifeTime--;
-    if (newDiv.lifeTime <= 30) {
-        newDiv.style.opacity = newDiv.lifeTime/30;
-    }
-    if (newDiv.lifeTime <= 0) {
-    chatRecord.removeChild(newDiv);
-    window.clearInterval(timer);
-    }
-},30);
+        newDiv.lifeTime--;
+        if (newDiv.lifeTime <= 30) {
+            newDiv.style.opacity = newDiv.lifeTime/30;
+            newDiv.style.height = ((newDiv.lifeTime/30)*40)+"px";
+        }
+        if (newDiv.lifeTime <= 0) {
+            chatRecord.removeChild(newDiv);
+            window.clearInterval(timer);
+        }
+    },30);
 }
+
 function chatBoxKeyDown(evt) {
-//When the user presses return
-if (evt.keyCode == 13) {
-receiveChat(chatBox.value,parseInt(chatTimer.value));
-chatBox.value = "";
+
+    //When the user presses return
+
+    if (evt.keyCode == 13) {
+        receiveChat(chatBox.value,parseInt(chatTimer.value));
+        chatBox.value = "";
+    }
 }
-}
+
 function onLoad() {
-chatRecord = document.getElementById("chatRecord");
-chatBox = document.getElementById("chatBox");
-chatTimer = document.getElementById("timer");
-chatBox.addEventListener("keydown",chatBoxKeyDown);
+    chatRecord = document.getElementById("chatRecord");
+    chatBox = document.getElementById("chatBox");
+    chatTimer = document.getElementById("timer");
+    chatBox.addEventListener("keydown",chatBoxKeyDown);
 }
+
 window.addEventListener("DOMContentLoaded",onLoad);
